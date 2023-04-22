24a094efd5cb115b40a40fa675492f42a5f7f8c3
Johnson Zhou <johnson.zhou@simplyuseful.io>
2023-04-22 10:34:13 +1000

    Explore: join claims with evidence Fixes #15

--- src/data.py
+++ src/data.py
@@ -3,8 +3,10 @@ Data I/O functions
 """
 import pathlib
 import pandas as pd
+from pandas import DataFrame
 import re
-from typing import DefaultDict
+import json
+from typing import List
 from collections import defaultdict
 
 # Colors for printing to the terminal
@@ -20,7 +22,17 @@ class COLORS:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def load_as_dataframe(names:list) -> DefaultDict[str, pd.DataFrame]:
+def load_from_json(names:list) -> List[dict]:
+    DATA_PATH = pathlib.Path("data", "*")
+    datasets = list()
+    for name in names:
+        with open(DATA_PATH.with_name(name).with_suffix(".json")) as json_file:
+            dataset = json.load(json_file)
+            datasets.append(dataset)
+            print(f"Loaded {name}")
+    return datasets
+
+def load_as_dataframe(names:list) -> List[DataFrame]:
     """
     Gets the json datasets as dataframes
 
@@ -30,34 +42,38 @@ def load_as_dataframe(names:list) -> DefaultDict[str, pd.DataFrame]:
     Returns:
         DefaultDict[str, pd.DataFrame]: Keys as names, values as the dataset
     """
-    DATA_PATH = pathlib.Path("data", "*")
-    datasets = defaultdict(pd.DataFrame)
-    for name in names:
-        with open(DATA_PATH.with_name(name).with_suffix(".json")) as f:
-            var_name = re.sub(pattern=r"-", repl="_", string=name)
-            df = pd.read_json(f, orient="index")
-            if name == "evidence":
-                df.columns = ["text"]
-            datasets[var_name] = df
-            print(f"Loaded {var_name}")
-    return datasets
+    # Load datasets from json as dict
+    datasets = load_from_json(names=names)
 
-def view_claim(claims:pd.DataFrame, evidence:pd.DataFrame) -> None:
-    """
-    Joins the claim and evidences and prints them for easier reading
+    # Process the names so that they are snake case
+    var_names = [re.sub(pattern=r"-", repl="_", string=name) for name in names]
 
-    Args:
-        claims (pd.DataFrame): The claims dataframe
-        evidence (pd.DataFrame): The evidence dataframe
-    """
-    for index, data in claims.iterrows():
-        print(COLORS.HEADER, f"{index} ==========", COLORS.END, "\n")
-        for key in ["claim_text", "claim_label", "evidences"]:
-            print(COLORS.OKBLUE, key, COLORS.END)
-            print(data[key], "\n")
-            if key == "evidences":
-                evidences = evidence.loc[data[key]]
-                for e_index, e_data in evidences.iterrows():
-                    print(COLORS.OKGREEN, e_index, COLORS.END)
-                    print(e_data["text"], "\n")
-    return
+    # Name the datasets
+    named_datasets = zip(var_names, datasets)
+
+    # Find the evidence if available
+    for var_name, dataset in named_datasets:
+        if var_name == "evidence":
+            df = DataFrame.from_dict(dataset, orient="index")
+            df.columns = ["evidence_text"]
+            locals()[var_name] = df
+
+    # Parse each dataset into a DataFrame then join evidence if available
+    datasets_df = list()
+    for var_name, dataset in zip(var_names, datasets):
+        if var_name == "evidence":
+            continue
+        df = DataFrame.from_dict(dataset, orient="index")
+        if "evidence" in locals().keys():
+            df = pd.merge(
+                left=df.explode(column="evidences"),
+                right=locals()["evidence"],
+                how="left",
+                left_on="evidences",
+                right_index=True
+            )
+        df = df.reset_index(names=["claim"]) \
+            .set_index(keys=["claim", "claim_text", "claim_label", "evidences"])
+        datasets_df.append(df)
+
+    return datasets_df
